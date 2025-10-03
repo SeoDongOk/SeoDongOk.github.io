e0af349a9609ea37ab77af3e0b38a3052d97ba4c
DongOk <hcan159@naver.com>
2025-10-03 15:55:18 +0900

    change legacy, darkmode color, remove tags,

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,10 +1,10 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'D.Ok Seo Blog',
+  author: 'DongOk Seo(Albert)',
+  headerTitle: 'D.Ok Seo Blog',
   description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  language: 'ko-kr',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
---
+++ layouts/test.js
@@ -0,0 +1,2 @@
+var test = 'This is a test layout'
+var teest2: String = 'This is a test layout 2'
